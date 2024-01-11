ac348e17f898b155291d80cd0ba44ec51539e9fe
Jade Irvine <jadesnotdeveloping@gmail.com>
2024-01-11 12:04:40 -0500

    create working `analyze` method

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,22 @@ const LexicalToken = union(LexicalTokenTag) {
     eof: void,
 };
 
+fn isDigit(input: u8) bool {
+    return input >= '0' and input <= '9';
+}
+
+fn printLexicalTokenList(list: []const LexicalToken) void {
+    std.debug.print("{{\n", .{});
+    for (list) |item| {
+        switch (item) {
+            LexicalToken.number => |number| std.debug.print("  Number({d}),\n", .{number}),
+            LexicalToken.plus => std.debug.print("  Plus,\n", .{}),
+            LexicalToken.eof => std.debug.print("  EOF,\n", .{}),
+        }
+    }
+    std.debug.print("}}\n", .{});
+}
+
 const Lexer = struct {
     cursor: usize,
     contents: []const u8,
@@ -25,9 +41,38 @@ const Lexer = struct {
         };
     }
 
-    pub fn analyze(self: *Self) []const LexicalToken {
-        _ = self;
-        return &[_]LexicalToken{};
+    pub fn analyze(self: *Self) ![]const LexicalToken {
+        var tokens = std.ArrayList(LexicalToken).init(std.heap.page_allocator);
+        while (self.peek(null)) |c| {
+            switch (c) {
+                '0'...'9' => {
+                    var numString = std.ArrayList(u8).init(std.heap.page_allocator);
+                    try numString.append(self.consume().?);
+                    while (self.peek(null)) |c2| {
+                        if (!isDigit(c2)) {
+                            break;
+                        }
+
+                        try numString.append(self.consume().?);
+                    }
+
+                    try tokens.append(.{
+                        .number = try std.fmt.parseInt(u32, numString.items, 10),
+                    });
+                },
+                '+' => {
+                    _ = self.consume();
+                    try tokens.append(LexicalToken.plus);
+                },
+                else => {
+                    _ = self.consume();
+                },
+            }
+        }
+
+        try tokens.append(LexicalToken.eof);
+
+        return tokens.items;
     }
 
     fn peek(self: Self, offset: ?usize) ?u8 {
@@ -37,7 +82,7 @@ const Lexer = struct {
         }
 
         const contentsOffset = self.cursor + cursorOffset;
-        if (contentsOffset > self.contents.len) {
+        if (contentsOffset >= self.contents.len) {
             return null;
         }
 
@@ -57,11 +102,13 @@ const Lexer = struct {
 pub fn main() !void {}
 
 test "can lex basic expression" {
-    var lexer = Lexer.new("5 + 7");
-    const analysis = lexer.analyze();
+    var lexer = Lexer.new("51 + 7");
+    const analysis = try lexer.analyze();
+
+    printLexicalTokenList(analysis);
 
     try std.testing.expectEqual(analysis.len, 4);
-    try std.testing.expectEqual(analysis[0], .{ .number = 5 });
+    try std.testing.expectEqual(analysis[0], .{ .number = 51 });
     try std.testing.expectEqual(@as(LexicalTokenTag, analysis[1]), LexicalTokenTag.plus);
     try std.testing.expectEqual(analysis[2], .{ .number = 7 });
     try std.testing.expectEqual(@as(LexicalTokenTag, analysis[3]), LexicalTokenTag.eof);
